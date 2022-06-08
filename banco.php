2aaf7406feea8678f5127a84d53074ddf1504f1a
LucasAugustoFatec <106602796+LucasAugustoFatec@users.noreply.github.com>
2022-06-07 21:29:06 -0300

    Update and rename conexao.php to banco.php

---
+++ banco.php
@@ -0,0 +1,14 @@
+<?php
+
+//timezone, ou seja, o horário colocado no site, ao que parece.
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+
+define('HOST', 'localhost');
+define('USUARIO', 'id18911902_main');
+define('SENHA', 'TW6Wp*/P]*^(z6oH');
+define('BANCO', 'id18911902_departamento_vendas');
+
+?>
--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'id18911902_main');
-define('SENHA', 'TW6Wp*/P]*^(z6oH');
-define('DB', 'id18911902_departamento_vendas');
-
-?>
-
-
